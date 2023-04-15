408a35edb3e0e64240c0b4877dee6a91172ecc8d
“antoanparashkevov” <tonkata1505@gmail.com>
2023-04-15 15:55:34 +0300

    create auth service

---
+++ src/services/authService.js
@@ -0,0 +1,79 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+//model
+import User from "../models/User.js";
+import user from "../models/User.js";
+
+export async function register(username, email, password) {
+    const existingUsername = await User.findOne({username : username})
+        .collation({
+            locale: 'en',
+            strength: 2//case insensitive
+        })
+    
+    if( existingUsername ) {
+        throw new Error('Username is taken!')
+    }
+    
+    const existingEmail = await User.findOne({email : email})
+        .collation({
+            locale: 'en',
+            strength: 2//case insensitive
+        })
+    
+    if( existingEmail ) {
+        throw new Error('Email is taken!')
+    }
+    
+    const hashedPassword = await bcrypt.hash(password, process.env['SALT'])
+    
+    const user = await User.create({
+        username,
+        email,
+        hashedPassword
+    })
+    
+    return createToken(user)
+    
+}
+
+export async function login(email, password) {
+    const existingEmail = await User.findOne({email : email})
+        .collation({
+            locale: 'en',
+            strength: 2//case insensitive
+        })
+    
+    if( !existingEmail ) {
+        throw new Error('Incorrect email or password!')
+    }
+    
+    const matchPassword = await bcrypt.compare(password, existingEmail.hashedPassword)//predicate -> returns true or false
+    
+    if( !matchPassword ) {
+        throw new Error('Incorrect email or password!')
+    }
+    
+    return createToken(existingEmail)
+    
+}
+
+function createToken({ username, email, _id, roles }) {
+    const payload = {
+        username,
+        email,
+        _id,
+        roles
+    }
+    
+    const token = jwt.sign(payload, process.env['JWT_SECRET'],{
+        expiresIn: process.env['TOKEN_EXPIRATION_TIME']
+    });
+    
+    return {
+        ...payload,
+        accessToken: token
+    }
+    
+}
