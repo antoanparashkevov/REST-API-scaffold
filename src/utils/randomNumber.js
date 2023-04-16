8f2d5efecbf8e379a36d6a06719f04e22df2d901
“antoanparashkevov” <tonkata1505@gmail.com>
2023-04-16 11:34:15 +0300

    randomNumber util func

---
+++ src/utils/randomNumber.js
@@ -0,0 +1,11 @@
+export default function(length) {
+    let text = '';
+    let possible = '123456789';
+    
+    for( let i = 0; i < length; i++) {
+        let sup = Math.floor(Math.random() * 9);
+        text +=  i > 0 && sup === i ? '0' : possible.charAt(sup)
+    }
+    
+    return Number(text);
+}
